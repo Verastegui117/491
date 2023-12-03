e255912ee95b34868fc190e9693a697977b5d9c3
Sean <spmccarthy4@gmail.com>
2023-12-02 19:26:56 -0800

    login/logut

--- server.py
+++ server.py
@@ -3,9 +3,30 @@ import requests
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from config import db_name
+import jwt
+
+def get_username_and_email_from_token(token):
+    try:
+        # Decode the token
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+
+        # Extract username and email
+        username = decoded_token.get('cognito:username')
+        email = decoded_token.get('email')
+
+        return username, email
+
+    except jwt.DecodeError:
+        # Handle invalid token
+        return "Invalid Token", None
+
+    except jwt.ExpiredSignatureError:
+        # Handle expired token
+        return "Token Expired", None
 
 app = Flask(__name__)
-app.secret_key = 'your-secret-ey' 
+
+app.secret_key = 'rand'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_name
 
@@ -27,10 +48,12 @@ def get_home():
 
 @app.route('/login', methods=['GET'])
 def login():
+    session['logged_in'] = True
     return redirect(f'https://cloud-proj.auth.us-east-1.amazoncognito.com/login?client_id=66jeh24nesfrshvc5n2q8n072q&response_type=code&scope=email+openid&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback')
 
 @app.route('/logout')
 def logout():
+    session.pop('logged_in', None)
     session.clear()
     return redirect(url_for('get_home')) 
 
@@ -49,8 +72,10 @@ def callback():
         response = requests.post(token_url, headers=headers, data=data)
         if response.status_code == 200:
             tokens = response.json()
-            session['id_token'] = tokens['id_token'] 
-            session['user'] = { 'email': 'extracted_email_from_id_token' }
+            session['id_token'] = tokens['id_token']
+            username, email = get_username_and_email_from_token(tokens['id_token'])
+            session['email'] = email
+ 
             return redirect(url_for('get_home'))
         else:
             return 'Error fetching tokens', 400
@@ -60,9 +85,9 @@ def callback():
 @app.route('/is_authenticated', methods=['GET'])
 def is_authenticated():
     if 'id_token' in session:
-        return jsonify({'authenticated': True}), 200
+        return True
     else:
-        return jsonify({'authenticated': False}), 401
+        return False
 
 @app.route('/contact', methods=['GET'])
 def get_contact():
@@ -92,21 +117,25 @@ def get_forum():
 
 @app.route('/add-comment', methods=['POST'])
 def add_comment():
-    data = request.form
 
-    # Validate input data
-    if not data or 'content' not in data or 'username' not in data:
-        return jsonify({'message': 'Missing required fields'}), 400
+    if not is_authenticated():
+        return redirect(url_for('login'))
+    else:
+        data = request.form
 
-    # Create a new comment instance
-    new_comment = Comments(
-        username=data['username'],
-        content=data['content']
-    )
-    db.session.add(new_comment)
-    db.session.commit()
+        # Validate input data
+        if not data or 'content' not in data or 'username' not in data:
+            return jsonify({'message': 'Missing required fields'}), 400
 
-    return redirect(url_for('get_comments'))
+        # Create a new comment instance
+        new_comment = Comments(
+            username=session['email'],
+            content=data['content']
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+
+        return redirect(url_for('get_comments'))
 
 @app.route('/comments')
 def get_comments():
